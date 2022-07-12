3645be396c9b6471df356fb0ee523ea7e4cae108
nfline <wufeicn@hotmail.com>
2022-07-12 02:09:41 -0400

    commit test

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-print ('welcome to nfline')
-
-
+print('welcome to nfline')
+##插入随机模块##
 import random
-chioce = ['a','ss','dd','ff']
+
+chioce = ['a', 'ss', 'dd', 'ff']
 
 print(random.choice(chioce))
+
+##测试主动换行但是输出时还是同一行##
+test = 'sasasadsdadsasasasasas'\
+    'asasaswqwqwq'\
+    'sawwwqqqq'
+print (test)
+
+##for循环##
+
+asd = 'nfline'
+
+for n in asd:
+    print(n)
+##if测试##
+if 'e' in asd:
+    print('e in asd nfline')
