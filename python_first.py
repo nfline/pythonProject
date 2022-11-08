5cc3b4cdae54577488cfedb0ff8808077acaee68
nfline <wufeicn@hotmail.com>
2022-11-07 21:32:54 -0500

    commit test

--- python_first.py
+++ python_first.py
@@ -1,4 +1,14 @@
+##在Windows和Linux下创建一个Python_First.py的代码，计算 1 + 2的结果，并且打印到屏幕上。
 a=1
 b=2
 
 print('计算1+2的结果\n'+'计算结果为:'+str(a+b))
+
+##创建一个随机产生IP地址的代码
+import random
+c=(random.randint(1,255))
+d=(random.randint(0,255))
+e=(random.randint(0,255))
+f=(random.randint(1,255))
+
+print(str(c)+'.'+str(d)+'.'+str(e)+'.'+str(f))
