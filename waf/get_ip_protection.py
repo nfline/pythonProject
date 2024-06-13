80e5404bab4b72faf31852c962c6a11286212cd8
nfline <93700321+nfline@users.noreply.github.com>
2024-06-12 23:54:25 -0400

    Update get_ip_protection.py

--- waf/get_ip_protection.py
+++ waf/get_ip_protection.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 
 # Load EP IDs from an Excel file
-df = pd.read_excel('applications.csv')
+df = pd.read_excel('applications.xlsx')
 ep_ids = df['ep_id'].tolist()  # Adjust the column name as necessary
 
 api_key = 'apikey'
@@ -20,10 +20,33 @@ for ep_id in ep_ids:
     response = requests.get(base_url_template.format(formatted_ep_id), headers=headers)
 
     if response.status_code == 200:
-        ip_protection_details = response.json()
-        # Add 'ep_id' to ip_protection_details for easier tracking
+        ip_protection_details = response.json().get('configs', {})
+        # Include 'ep_id' in the data
         ip_protection_details['ep_id'] = formatted_ep_id
+
+        # Handle 'block_country_list'
+        ip_protection_details['block_country_list'] = ', '.join(ip_protection_details.get('block_country_list', []))
+
+        # Process 'ip_list'
+        trust_ip = []
+        block_ip = []
+        allow_only_ip = []
+        for ip_entry in ip_protection_details.get('ip_list', []):
+            if ip_entry['type'] == 'trust-ip':
+                trust_ip.append(ip_entry['ip'])
+            elif ip_entry['type'] == 'block-ip':
+                block_ip.append(ip_entry['ip'])
+            elif ip_entry['type'] == 'allow-only-ip':
+                allow_only_ip.append(ip_entry['ip'])
+
+        ip_protection_details['trust_ips'] = ', '.join(trust_ip)
+        ip_protection_details['block_ips'] = ', '.join(block_ip)
+        ip_protection_details['allow_only_ips'] = ', '.join(allow_only_ip)
+
+        # Clean up the original keys if needed
+        ip_protection_details.pop('ip_list', None)
         all_ip_protection_details.append(ip_protection_details)
+        print(f"Success to fetch IP protection details for EP ID {ep_id}: {response.status_code}")
     else:
         print(f"Failed to fetch IP protection details for EP ID {ep_id}: {response.status_code}")
 
